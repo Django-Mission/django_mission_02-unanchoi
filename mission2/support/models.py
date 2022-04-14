0263a0961860721290638e598719dd49458ad3b7
unanchoi <unusualc@likelion.org>
2022-04-15 00:32:07 +0900

    feat : Add faq model

--- mission2/support/models.py
+++ mission2/support/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Faq(models.Model):
+    question = models.TextField()
+    category = models.CharField(
+        max_length=30,
+        default='mission',
+        choices=
+
+                                [
+                                    ('python', 'python'),
+                                    ('mission', 'mission'),
+                                    ('django' , 'django')
+                                ]
+    )
+    answer = models.TextField()
+    writer = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    editor = models.CharField(max_length=50)
+    edited_at = models.DateTimeField(auto_now=True)
